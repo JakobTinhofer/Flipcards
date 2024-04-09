783aa9f67771feba19ca2a0c797d5514edf5117d
Jakob Tinhofer <65621944+JakobTinhofer@users.noreply.github.com>
2024-04-09 20:28:45 +0200

    removed exit

--- modules/physik_grundlagen/build_gdp1b.py
+++ modules/physik_grundlagen/build_gdp1b.py
@@ -5,8 +5,7 @@ import requests, os, json
 NOTION_URL = "https://api.notion.com/v1/"
 NOTION_DB_ID = "183155c647e84e7ca801f15e259f82bd"
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
-if not NOTION_TOKEN:
-    quit(-1)
+
 MY_PATH = globals().get("MODULE_FOLDER", "missing")
 if MY_PATH != "missing":
     __file__ = "A"
